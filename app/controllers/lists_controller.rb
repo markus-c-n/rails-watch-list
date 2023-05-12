cc6934f1964828250a42757a2f0b559980961816
Markus Nowak <markusnowak99@gmail.com>
2023-05-12 15:25:21 +0900

    ??

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,7 +17,7 @@ class ListsController < ApplicationController
 
   def destroy
     @list = List.find(params[:id])
-    @list.delete
+    @list.destroy
     redirect_to lists_path, status: :see_other
   end
 
